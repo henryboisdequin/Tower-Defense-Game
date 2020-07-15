a23302bda0132f9541a9084c21de63803ae725f7
Henry Boisdequin <65845077+henryboisdequin@users.noreply.github.com>
2020-07-15 16:16:10 +0530

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,134 @@
+import pygame
+import os
+from button import Button
+
+
+star = pygame.transform.scale(pygame.image.load(os.path.join("Game/Shop", "star.png")).convert_alpha(), (50, 50))
+star2 = pygame.transform.scale(pygame.image.load(os.path.join("Game/Shop", "star.png")).convert_alpha(), (20, 20))
+
+
+class VerticalButton(Button):
+    def __init__(self, x, y, img, name, cost):
+        self.name = name
+        self.img = img
+        self.x = x
+        self.y = y
+        self.width = self.img.get_width()
+        self.height = self.img.get_height()
+        self.cost = cost
+
+
+class Menu:
+    def __init__(self, tower, x, y, img, item_cost):
+        self.x = x
+        self.y = y
+        self.width = img.get_width()
+        self.height = img.get_height()
+        self.item_cost = item_cost
+        self.buttons = []
+        self.items = 0
+        self.bg = img
+        self.font = pygame.font.SysFont("comicsans", 25)
+        self.tower = tower
+
+    def add_btn(self, img, name):
+        """
+        adds buttons to menu
+        :param img: surface
+        :param name: str
+        :return: None
+        """
+        self.items += 1
+        self.buttons.append(Button(self, img, name))
+
+    def get_item_cost(self):
+        """
+        gets cost of upgrade to next level
+        :return: int
+        """
+        return self.item_cost[self.tower.level - 1]
+
+    def draw(self, win):
+        """
+        draws btns and menu bg
+        :param win: surface
+        :return: None
+        """
+        win.blit(self.bg, (self.x - self.bg.get_width()/2, self.y-120))
+        for item in self.buttons:
+            item.draw(win)
+            win.blit(star, (item.x + item.width + 5, item.y-9))
+            text = self.font.render(str(self.item_cost[self.tower.level - 1]), 1, (255,255,255))
+            win.blit(text, (item.x + item.width + 30 - text.get_width()/2, item.y + star.get_height() -8))
+
+    def get_clicked(self, X, Y):
+        """
+        return the clicked item from the menu
+        :param X: int
+        :param Y: int
+        :return: str
+        """
+        for btn in self.buttons:
+            if btn.clicked(X, Y):
+                return btn.name
+
+        return None
+
+    def update(self):
+        """
+        update menu and button location
+        :return: None
+        """
+        for btn in self.buttons:
+            btn.update()
+
+
+class VerticalMenu(Menu):
+    """
+    Vertical Menu for side bar of game
+    """
+    def __init__(self, x, y, img):
+        self.x = x
+        self.y = y
+        self.width = img.get_width()
+        self.height = img.get_height()
+        self.buttons = []
+        self.items = 0
+        self.bg = img
+        self.font = pygame.font.SysFont("comicsans", 25)
+
+    def add_btn(self, img, name, cost):
+        """
+        adds buttons to menu
+        :param img: surface
+        :param name: str
+        :return: None
+        """
+        self.items += 1
+        btn_x = self.x - 40
+        btn_y = self.y-100 + (self.items-1) * 120
+        self.buttons.append(VerticalButton(btn_x, btn_y, img, name, cost))
+
+    def get_item_cost(self, name):
+        """
+        Gets cost of item
+        :param name: str
+        :return: int
+        """
+        for btn in self.buttons:
+            if btn.name == name:
+                return btn.cost
+        return -1
+
+    def draw(self, win):
+        """
+        draws buttons and menu bg.
+        :param win: surface
+        :return: None
+        """
+        win.blit(self.bg, (self.x - self.bg.get_width()/2, self.y-120))
+        for item in self.buttons:
+            item.draw(win)
+            win.blit(star2, (item.x+2, item.y + item.height))
+            text = self.font.render(str(item.cost), 1, (255, 255, 255))
+            win.blit(text, (item.x + item.width/2 - text.get_width()/2 + 7, item.y + item.height + 5))
